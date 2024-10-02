b451096c16f082b691e9ff3dce0fd4d7e66a5307
220711927 <220711927@students.uajy.ac.id>
2024-10-02 11:16:01 +0700

    Login  ubah warna
    buat login

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Cobain'),
+          child: ElevatedButton(
+            onPressed: () {
+              print('Tekan');
+            },
+            child: const Text(
+              'Cobain',
+              style: TextStyle(color: Color.fromARGB(255, 138, 97, 82)),
+            ),
+          ),
         ),
       ),
     );
