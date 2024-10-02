87affc69251a02559e68ec6fd73ff9e96cbec7f7
krisnadewaaa <220711838@students.uajy.ac.id>
2024-10-02 10:56:57 +0700

    Membuat Profile Page
    Profile Page versi 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:a_salon/View/profile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Dewa - 220711838!'),
-        ),
-      ),
+      home: ProfilePage(),
     );
   }
 }
