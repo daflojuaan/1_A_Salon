8c20a2e3eb28d1336ad68b8ad55ac737de61e9fa
YohanesPaulusArdha <220711658@students.uajy.ac.id>
2024-10-02 11:00:07 +0700

    membuat list reservasi
    membuat list reservasi yang dibuat user tetapi blm selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:a_salon/View/listreservasi.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('220711658-Yohanes Paulus Ardha'),
-        ),
-      ),
+      home: ReservasiView(),
     );
   }
 }
