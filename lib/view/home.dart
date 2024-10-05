568b4841276abbe74b77c9928db243566b12b621
marcello220711844 <220711844@students.uajy.ac.id>
2024-10-05 17:42:18 +0700

    Membuat Home Page
    membuat home page yang membutuhkan code tampilanProfile dan code reservasi.

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:a_salon/view/tampilanProfile.dart';
+import 'package:a_salon/view/reservasi.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+  final Map<String, dynamic>? profileData;
+
+  const HomeView({super.key, this.profileData});
 
   @override
   State<HomeView> createState() => _HomeViewState();
@@ -9,6 +13,8 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   int _selectedIndex = 0;
+  bool _isBalanceVisible = true;
+  late List<Widget> _widgetOptions;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -16,90 +22,179 @@ class _HomeViewState extends State<HomeView> {
     });
   }
 
-  static const List<Widget> _widgetOptions = <Widget>[
-    Center(child: Text('Home Page')),
-    Center(child: Text('Order List')),
-    Center(
-      child: Text(
-        'Index 3: Profile',
+  void initState() {
+    super.initState();
+    _widgetOptions = <Widget>[
+      const Center(
       ),
-    ),
-  ];
+      const Center(),
+      ProfileView(user: widget.profileData),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome Nama'),
-        backgroundColor: Colors.red,
+        title: const Text(
+          'Selamat Datang',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF001F3F),
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage('https://picsum.photos/200'),
-            radius: 20,
+          child: Image.asset(
+            'images/logo putih.jpg',
+            width: 500,
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50.0),
-            child: Container(
-              height: 100,
-              width: 400,
-              color: Colors.blue,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 58, 109, 140),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Saldo',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            _isBalanceVisible ? 'Rp. 0' : '•••',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              _isBalanceVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isBalanceVisible = !_isBalanceVisible;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                        ),
+                        onPressed: () {},
+                        child: const Text('+ Top Up Saldo'),
+                      ),
+                      const SizedBox(height: 10),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Point',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            'Bonus',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Menu
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: const Text(
-                      'Saldo: Rp 1.000.000',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      ' Menu',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
                     ),
                   ),
+                  const SizedBox(height: 15),
                   Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: const Text(
-                      'Member Gold',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        _buildMenuItem(Icons.payment, 'Pembayaran\nSaya', () {
+                          print('Pembayaran Saya Tapped');
+                        }),
+                        _buildMenuItem(Icons.calendar_today, 'Reservasi\n', () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ReservasiView()),
+                          );
+                        }),
+                        _buildMenuItem(Icons.feedback, 'Keluhan\nPelanggan',
+                            () {
+                          print('Keluhan Pelanggan Tapped');
+                        }),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30.0),
-            child: Container(
-              height: 450,
-              width: 450,
-              color: const Color.fromARGB(255, 29, 6, 154),
-              alignment: Alignment.topLeft,
-              child: const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  'Promo HairCut',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-              ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: _buildPromoSection('Promo HairCut', ['Promo 1']),
             ),
-          ),
-          Expanded(
-            child: _widgetOptions.elementAt(_selectedIndex),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: _buildPromoSection('Promo HairCut', ['Promo 2']),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: _buildPromoSection('Promo HairCut', ['Promo 3']),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: _widgetOptions.elementAt(_selectedIndex),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 0, 31, 63),
+        selectedItemColor: const Color.fromARGB(234, 216, 177, 0),
+        unselectedItemColor: Colors.white,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.cut_sharp),
-            label: 'Order',
+            icon: Icon(Icons.qr_code_scanner_outlined),
+            label: 'Scan',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
@@ -108,6 +203,70 @@ class _HomeViewState extends State<HomeView> {
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _buildMenuItem(IconData icon, String label, VoidCallback onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.black,
+            radius: 30,
+            child: Icon(icon, size: 30, color: Colors.white),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPromoSection(String title, List<String> promoTitles) {
+    return Container(
+      height: 200,
+      width: 450,
+      color: const Color(0xFF001F3F),
+      alignment: Alignment.topLeft,
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'Promo HairCut',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: _buildPromoBox(promoTitles[0]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPromoBox(String title) {
+    return Container(
+      width: 400,
+      height: 110,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.0),
+        border: Border.all(color: Colors.black),
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.black),
+        ),
       ),
     );
   }
