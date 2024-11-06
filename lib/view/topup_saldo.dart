3c0eff5932f3a7c29a31e3fb1bf37250cbf05b88
GabrielHazel <220711906@students.uajy.ac.id>
2024-11-06 19:19:44 +0700

    buat register, login, main, user, navbar,  scan

---
+++ lib/view/topup_saldo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TopUpSaldoPage extends StatefulWidget {
+  const TopUpSaldoPage({super.key});
+
+  @override
+  State<TopUpSaldoPage> createState() => _TopUpSaldoPageState();
+}
+
+class _TopUpSaldoPageState extends State<TopUpSaldoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
